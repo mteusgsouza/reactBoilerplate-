db39c5c28f6f5000a901ee1c012fcdbc226c1950
mateusgsouza <mteus.g.souza@gmail.com>
2020-05-24 22:54:31 -0300

    authentication firebase

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { withAuthorization } from '../Session';
  
-const Home = () => (
+const HomePage = () => (
   <div>
     <h1>Home</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
   </div>
 );
+
+const condition = authUser => !!authUser;
  
-export default Home;
+export default withAuthorization(condition)(HomePage);
